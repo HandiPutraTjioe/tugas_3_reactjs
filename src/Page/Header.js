7ec7dba2dd4cc8bc9ac81e06da9a924aa55b480a
HandiPutraTjioe <handibncc23@gmail.com>
2020-03-14 18:39:36 +0700

    Tugas 3 React Js

---
+++ src/Page/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+    render(){
+        return(
+            <div>
+                <center>
+                    <b>
+                        <td>Home |&nbsp;</td>
+                        <td>Menu |&nbsp;</td>
+                        <td>Tentang Kami |&nbsp;</td>
+                        <td>Kontak Kami</td>
+                    </b>
+                </center>
+            </div>
+        );
+    }
+}
+
+export default Header;
---
+++ src/Page/MenuMakanan.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import ListMakanan from '../ListData/ListMakanan';
+
+class MenuMakanan extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            adds: 0
+        };
+
+        this.rubahPesanan = this.rubahPesanan.bind(this);
+        this.pesanan = this.pesanan.bind(this);
+    }
+
+    rubahPesanan(){
+        this.setState((state, props) => {
+            return{
+                pesan: state.adds
+            };
+        });
+    }
+
+    pesanan(e){
+        console.log(e.target.value);
+    }
+
+    render(){
+        return(
+            <div>
+                <h3>Daftar Makanan Yang Kami Sediakan :</h3>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <ListMakanan litsmkn='https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/nasipadang.jpg'/>
+                                <center>
+                                    <button onClick={this.rubahPesanan}>Pesan Sekarang</button>
+                                </center>
+                            </td>
+
+                            <td>
+                                <ListMakanan litsmkn='https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png'/>
+                                <center>
+                                    <button onClick={this.rubahPesanan}>Pesan Sekarang</button>
+                                </center>
+                            </td>
+
+                            <td>
+                                <ListMakanan litsmkn='https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sotolamongan.png'/>
+                                <center>
+                                    <button onClick={this.rubahPesanan}>Pesan Sekarang</button>
+                                </center>
+                            </td>
+
+                            <td>
+                                <ListMakanan litsmkn='https://www.dbs.com/iwov-resources/images/newsroom/indonesia/Blog/masakan%20nusantara/nasi%20kentut.png'/>
+                                <center>
+                                    <button onClick={this.rubahPesanan}>Pesan Sekarang</button>
+                                </center>
+                            </td>
+
+                            <td>
+                                <ListMakanan litsmkn='https://pesona.travel/media/nasi-kuning-sarapan-khas-nusantara-bermakna-mendalam_144651_1140.jpg'/>
+                                <center>
+                                    <button onClick={this.rubahPesanan}>Pesan Sekarang</button>
+                                </center>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <br />
+                <input type="text" onChange={this.pesanan} />
+                <h3>Pesanan Anda : {this.state.adds}</h3>
+            </div>
+        );
+    }
+}
+
+export default MenuMakanan;
